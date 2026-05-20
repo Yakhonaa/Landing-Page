d136bf3758b8df526fd6707e6bdb38e043c776aa
max <karambit.max@gmail.com>
2026-05-20 19:21:56 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+function App() {
+  return (
+    <main className="min-h-screen">
+      <section>
+        <div className="first-header">
+          <div className="left-header">
+            <div className="left-header-content">
+            <h className="left-header-content-title">Hire and Offer</h><br></br>
+            <h className="left-header-content-subtitle">with <span className="Loochon-highlight">Loochon</span></h>
+            </div>
+            <div className="left-header-content-description">
+              <p>Loochon is a platform that allows you to hire local professionals and offer your skills to the world.</p>
+            </div>
+          </div>
+          <div className="right-header">
+            <div className="right-header-content">
+              <div className="offer-image-container">
+              <img src="public\offer.jpg" className="offer-image"/>
+              <img src="public\hire.jpg" className="hire-image"/>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  )
+}
+
+export default App
