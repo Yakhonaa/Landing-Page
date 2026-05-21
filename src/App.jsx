56c86080c128ebe2d5986ba5c3484e494ab20837
max <karambit.max@gmail.com>
2026-05-22 00:20:39 +0300

    nice version mobile

--- src/App.jsx
+++ src/App.jsx
@@ -11,20 +11,20 @@ function App() {
             <span></span>
           </button>
         </div>
-        <div className="first-header">
-          <div className="left-header">
-            <div className="left-header-content">
+        <div className="top-header-content">
               <h1 className="left-header-content-title">Hire and Offer</h1>
               <h2 className="left-header-content-subtitle">
                 with <span className="Loochon-highlight">Loochon</span>
               </h2>
             </div>
+        <div className="first-header">
+          
+          <div className="left-header">
+            
             <div className="left-header-content-description">
               <p>Loochon is a platform that allows you to hire local professionals and offer your skills to the world.</p>
             </div>
-            <div className="left-header-content-buttons">
-              <button className="left-header-content-button">Get Started</button>
-            </div>
+            
           </div>
           <div className="right-header">
             <div className="right-header-content">
@@ -32,12 +32,15 @@ function App() {
                 <div className="offer-image-container">
                   <img src="/offer.jpg" className="offer-image" alt="Offer preview" />
                 </div>
-                <div className="hire-image-container">
-                  <img src="/hire.jpg" className="hire-image" alt="Hire preview" />
-                </div>
+                
               </div>
             </div>
           </div>
+        </div>
+        <div className="left-header-content-buttons-container">
+          <div className="left-header-content-buttons">
+              <button className="left-header-content-button">Get Started</button>
+            </div>
         </div>
         <div className="stripe-wrapper">
   <div className="diagonal-stripe"></div>
